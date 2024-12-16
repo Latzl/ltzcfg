342e16e1c7dda6312bd585c0350b0ce49f8bdb97
laozh <laozh@infogo.com.cn>
2024-12-16 17:08:47 +0800

    rearrange directory structure

--- xxh/install.sh
+++ xxh/install.sh
@@ -7,27 +7,29 @@ fi
 
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 LTZCFG_ROOT_DIR="$(realpath ${CURR_DIR}/..)"
-XHH_HOME=~/.xxh
+LTZCFG_XXH_HOME="${LTZCFG_ROOT_DIR}/xxh/home/.xxh"
+XXH_HOME=~/.xxh
 
-xxh +RI xxh-shell-bash+path+${CURR_DIR}/shells/xxh-shell-bash
-xxh +RI xxh-plugin-bash-basic+path+${CURR_DIR}/plugins/xxh-plugin-bash-basic
-xxh +RI xxh-plugin-bash-example+path+${CURR_DIR}/plugins/xxh-plugin-bash-example
+PLUGIN_NAME='xxh-shell-bash'
+xxh +RI ${PLUGIN_NAME}+path+${LTZCFG_XXH_HOME}/.xxh/shells/${PLUGIN_NAME}
+PLUGIN_NAME='xxh-plugin-bash-basic'
+xxh +RI ${PLUGIN_NAME}+path+${LTZCFG_XXH_HOME}/.xxh/plugins/${PLUGIN_NAME}
+PLUGIN_NAME='xxh-plugin-bash-example'
+xxh +RI ${PLUGIN_NAME}+path+${LTZCFG_XXH_HOME}/.xxh/plugins/${PLUGIN_NAME}
 
 # dotfiles
+shopt -s dotglob
 # symbolic link
 PLUGIN_NAME='xxh-plugin-prerun-dotfiles-symbolic-link'
-DST_DIR="${XHH_HOME}/.xxh/plugins/${PLUGIN_NAME}/build/home"
-xxh +RI ${PLUGIN_NAME}+path+${CURR_DIR}/plugins/${PLUGIN_NAME}
-cp -iv "${LTZCFG_ROOT_DIR}/bash/.bash_profile" "${DST_DIR}/"
-cp -ivr "${LTZCFG_ROOT_DIR}/bash/.bash_profile.d" "${DST_DIR}/"
-cp -iv "${LTZCFG_ROOT_DIR}/bash/.bashrc" "${DST_DIR}/"
-cp -ivr "${LTZCFG_ROOT_DIR}/bash/.bashrc.d" "${DST_DIR}/"
-cp -iv "${LTZCFG_ROOT_DIR}/vim/.vimrc" "${DST_DIR}/"
-cp -iv "${LTZCFG_ROOT_DIR}/tmux/.tmux.conf" "${DST_DIR}/"
-cp -iv "${LTZCFG_ROOT_DIR}/tmux/.tmux.conf.plugins" "${DST_DIR}/"
+DST_DIR="${XXH_HOME}/.xxh/plugins/${PLUGIN_NAME}/build/home"
+xxh +RI ${PLUGIN_NAME}+path+${LTZCFG_XXH_HOME}/.xxh/plugins/${PLUGIN_NAME}
+cp -ir ${LTZCFG_ROOT_DIR}/bash/home/* "${DST_DIR}/"
+cp -ir ${LTZCFG_ROOT_DIR}/vim/home/* "${DST_DIR}/"
+cp -ir "${LTZCFG_ROOT_DIR}/tmux/home/.tmux.conf" "${DST_DIR}/"
+cp -ir "${LTZCFG_ROOT_DIR}/tmux/home/.tmux.conf.plugins" "${DST_DIR}/"
 
 # put once
 PLUGIN_NAME='xxh-plugin-prerun-dotfiles-put-once'
-DST_DIR="${XHH_HOME}/.xxh/plugins/${PLUGIN_NAME}/build/home"
-xxh +RI ${PLUGIN_NAME}+path+${CURR_DIR}/plugins/${PLUGIN_NAME}
-cp -ivr "${LTZCFG_ROOT_DIR}/tmux/.tmux" "${DST_DIR}/"
+DST_DIR="${XXH_HOME}/.xxh/plugins/${PLUGIN_NAME}/build/home"
+xxh +RI ${PLUGIN_NAME}+path+${LTZCFG_XXH_HOME}/.xxh/plugins/${PLUGIN_NAME}
+cp -ir "${LTZCFG_ROOT_DIR}/tmux/home/.tmux" "${DST_DIR}/"
