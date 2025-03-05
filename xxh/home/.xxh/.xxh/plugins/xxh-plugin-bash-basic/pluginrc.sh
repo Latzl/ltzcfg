160c11448e7f16cf95ca6e801370b48fdeafcc0f
latzl <theaccount_latzl@163.com>
2025-03-06 01:00:09 +0800

    remove xxh

--- xxh/home/.xxh/.xxh/plugins/xxh-plugin-bash-basic/pluginrc.sh
+++
@@ -1,8 +0,0 @@
-CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-if [[ $XXH_VERBOSE == '2' ]]; then
-  echo 'Load xxh-plugin-bash-basic'
-fi
-
-[ -f /etc/profile ] && source /etc/profile
-[ -f ~/.bash_profile ] && source ~/.bash_profile
--- xxh/home/.xxh/.xxh/plugins/xxh-plugin-bash-example/pluginrc.sh
+++
@@ -1,9 +0,0 @@
-CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-if [[ $XXH_VERBOSE == '2' ]]; then
-  echo 'Load xxh-plugin-bash-example'
-fi
-
-ltz-xxh-bash-example(){
-	echo 'ltz-xxh-bash-example'
-}
